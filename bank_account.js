2950238f9f1dc0be9c098ecfc05b9ed3338bc83d
Ahmad Subhan Daryhadi <140992677+ahmadsubhand@users.noreply.github.com>
2024-09-27 22:19:05 +0700

    memperbaiki-bank_account

--- bank_account.js
+++ bank_account.js
@@ -33,17 +33,17 @@ function errorMessage(error) {
 
 class BankAccount {
   constructor(owner, balance = 0) {
-    this._owner = owner;
-    this._balance = balance;
-    console.log(`Welcome ${_owner}!\n${this.getBalance()}\n`);
+    this.owner = owner;
+    this.balance = balance;
+    console.log(`Welcome ${owner}!\n${this.getBalance()}\n`);
   }
 
   deposit(amount) {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
         if (amount > 0) {
-          this._balance += amount;
-          resolve(`Deposit of ${amount} successful. New balance: ${this._balance}.\n`);
+          this.balance += amount;
+          resolve(`Deposit of ${amount} successful. New balance: ${this.balance}.\n`);
         } else if (amount <= 0) {
           reject(new InvalidTransactionError(`Deposit amount must be greater than zero.\n`));
         } else {
@@ -56,15 +56,15 @@ class BankAccount {
   withdraw(amount) {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
-        if (amount > 0 && amount <= this._balance) {
-          this._balance -= amount;
-          resolve(`Withdraw of ${amount} successful. New balance: ${this._balance}.\n`);
-        } else if (this._balance == 0) {
+        if (amount > 0 && amount <= this.balance) {
+          this.balance -= amount;
+          resolve(`Withdraw of ${amount} successful. New balance: ${this.balance}.\n`);
+        } else if (this.balance == 0) {
           reject(new InsufficientFundsError(`You tried to withdraw, but your balance is empty.\n`))
         } else if (amount <= 0) {
           reject(new InvalidTransactionError(`Withdraw amount must be greater than zero.\n`));
-        } else if (amount > this._balance) {
-          reject(new InsufficientFundsError(`You tried to withdraw ${amount}, but your balance is only ${this._balance}.\n`));
+        } else if (amount > this.balance) {
+          reject(new InsufficientFundsError(`You tried to withdraw ${amount}, but your balance is only ${this.balance}.\n`));
         } else {
           reject(new DataValidationError(`Please enter a valid number.\n`));
         }
@@ -72,12 +72,8 @@ class BankAccount {
     });
   }
 
-  cekSaldo() {
-    window.alert(`Saldo yang Anda miliki saat ini sebesar ${this.saldo}`);
-  }
-
   getBalance() {
-    return `Current balance: ${this._balance}`;
+    return `Current balance: ${this.balance}`;
   }
 }
 
