89594f02b39ec176ddbdc4c2063a1858efe66ad8
Ahmad Subhan <asdcode123@gmail.com>
2024-09-27 21:20:11 +0700

    memasukkan-file-dari-chapter-1

---
+++ bank_account.js
@@ -0,0 +1,74 @@
+class BankAccount {
+  constructor(saldo, nama) {
+    this.saldo = saldo
+    this.nama = nama
+  }
+
+  tambahSaldo() {
+    let next = true;
+    let pesan = "";
+    do {
+      let input = parseFloat(window.prompt("Masukkan jumlah saldo yang ingin ditambahkan"))
+      if (!isNaN(input)) {
+        this.saldo += input
+        pesan = `Saldo sebesar ${input} telah ditambahkan!`
+        next = false;
+      } else {
+        pesan = `Jumlah yang dimasukkan tidak valid!`
+      }
+      window.alert(pesan)
+    } while(next)
+    this.cekSaldo();
+  }
+
+  kurangiSaldo() {
+    let next = true;
+    let pesan = "";
+    do {
+      let input = parseFloat(window.prompt("Masukkan jumlah saldo yang ingin diambil"))
+      if (!isNaN(input) && input <= this.saldo) {
+        this.saldo -= input
+        pesan = `Saldo sebesar ${input} telah diambil!`
+        next = false;
+      } else if (input > this.saldo) {
+        pesan = `Saldo tidak mencukupi!`
+      } else {
+        pesan = `Jumlah yang dimasukkan tidak valid!`
+      }
+      window.alert(pesan)
+    } while(next)
+    this.cekSaldo();
+  }
+
+  cekSaldo() {
+    window.alert(`Saldo yang Anda miliki saat ini sebesar ${this.saldo}`);
+  }
+}
+
+window.onload = () => {
+  let next = true;
+  let nasabah = new BankAccount(0, "Ahmad Subhan Daryhadi");
+  do {
+    let pilihan = parseInt(window.prompt(
+      `Menu\n1. Cek Saldo\n2. Tambah Saldo\n3. Kurang saldo\n4. Keluar\nPilih menu yang akan Anda lakukan...`
+    ))
+    switch (pilihan) {
+      case 1:
+        nasabah.cekSaldo();
+        break;
+      case 2:
+        nasabah.tambahSaldo();
+        break;
+      case 3:
+        nasabah.kurangiSaldo();
+        break;
+      case 4:
+        window.alert("Terimakasih sudah menggunakan aplikasi!")
+        next = false;
+        break;
+      default:
+        window.alert("Silahkan masukkan angka yang valid! (1-4)")
+        break;
+    }
+  } while(next)
+}
