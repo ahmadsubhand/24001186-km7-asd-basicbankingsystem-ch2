5a7b123537f151a613481f88b402f3ab66895909
Ahmad Subhan <asdcode123@gmail.com>
2024-09-27 22:31:35 +0700

    membuat-pilihan-menu

--- banking_system.js
+++ banking_system.js
@@ -9,6 +9,57 @@ async function main() {
   );
 
   const account = new BankAccount(name, balance);
+
+  let next = 1;
+  do {
+    const choice = parseInt(await readInput("Menu\n1. Withdraw\n2. Deposit\n3. Check Balance\nPlease select what you want to do: "));
+
+    switch (choice) {
+      case 1: {
+        const amount = parseFloat(await readInput("Please enter the amount to be withdrawn: "));
+
+        try {
+          const result = await account.withdraw(amount);
+          console.log(result);
+        } catch (error) {
+          console.log(errorMessage(error));
+        }
+
+        break;
+      }
+      
+      case 2: {
+        const amount = parseFloat(await readInput("Please enter the amount to be deposited: "));
+
+        try {
+          const result = await account.deposit(amount);
+          console.log(result);
+        } catch (error) {
+          console.log(errorMessage(error));
+        }
+
+        break;
+      }
+
+      case 3: {
+        console.log(account.getBalance());
+        break;
+      }
+      
+      default:
+        console.error("<!> Invalid data: Please enter a number between 1-3.\n");
+        break;
+    }
+
+    next = parseInt(await readInput('Do you want to make another transaction or exit app? (1/0) '));
+    while (next != 0 && next != 1) {
+      console.error(`<!> Invalid data: Please enter a number between 1 or 0.\n`);
+      next = parseInt(await readInput('Do you want to make another transaction or exit app? (1/0) '));
+    }
+
+  } while(next);
+
+  console.log(`Thanks for using this app. See you again ${name}!`);
   closeInput();
 }
 
